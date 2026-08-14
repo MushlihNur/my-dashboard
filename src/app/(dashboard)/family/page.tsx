c027645a19d738761526c50811e45b10b3071139
MushlihNur <mushlih.nuris@gmail.com>
2026-08-14 14:38:34 +0700

    feat: add placeholder for profile, family, & docs page

---
+++ src/app/(dashboard)/family/page.tsx
@@ -0,0 +1,11 @@
+export default function FamilyPage() {
+  return (
+    <div className="flex items-center justify-center h-full py-24">
+      <div className="text-center flex flex-col gap-2">
+        <p className="text-2xl">🚧</p>
+        <p className="text-sm font-medium text-c3">Coming Soon</p>
+        <p className="text-xs text-c2">This page is currently under development.</p>
+      </div>
+    </div>
+  )
+}
